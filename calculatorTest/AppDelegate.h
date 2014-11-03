82e3cf4eef878ee49bd957d4a598c12ceb6ce3b0
nong <m13354282530@163.com>
2014-11-03 18:03:02 +0800

    Initial Commit

---
+++ calculatorTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculatorTest
+//
+//  Created by nong on 14/11/3.
+//  Copyright (c) 2014年 nong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calculatorTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calculatorTest
+//
+//  Created by nong on 14/11/3.
+//  Copyright (c) 2014年 nong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
